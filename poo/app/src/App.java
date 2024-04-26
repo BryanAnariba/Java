74bf66f2da75e5badf5991b1b1318ce95f013568
BryanAnariba <saariel115@gmail.com>
2024-04-26 06:32:40 -0600

    POO PART 1

--- poo/app/src/App.java
+++ poo/app/src/App.java
@@ -1,13 +1,16 @@
+import classes.Carrer;
 import classes.Person;
-
+3 52
 public class App {
     public static void main(String[] args) throws Exception {
         String[] tecnologies = new String[3];
         tecnologies[0] = "Node";
         tecnologies[1] = "Javascript";
         tecnologies[2] = "Angular";
-        Person studentOne = new Person("Student One", "Last Name One", 20, true, tecnologies);
 
-        System.out.println(studentOne);
+        Person studentOne = new Person("Student One", "Last Name One", 20, true, tecnologies);
+        Carrer studentOneCarrer = new Carrer("Systems Enginner", 5);
+
+        System.out.println(studentOne + " " + studentOneCarrer);
     }
 }
