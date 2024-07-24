722958a255d0eeaa54ad0f61c9e046b73e5540d5
BryanAnariba <saariel115@gmail.com>
2024-07-24 07:04:10 -0600

    Adding student practice part 2

--- fundamentals/StudentsExcercise/src/main/java/classes/Student.java
+++ fundamentals/StudentsExcercise/src/main/java/classes/Student.java
@@ -1,5 +1,7 @@
 package classes;
 
+import java.util.Scanner;
+
 public class Student {
   private int age;
   private String account;
@@ -57,6 +59,21 @@ public class Student {
 
   public void setCarrer(String carrer) {
     this.carrer = carrer;
+  }
+  
+  public void addNewStudent (Scanner inputKeyboard) {
+    System.out.println("===ADDING NEW STUDENT===");
+    System.out.println((this.account != null) ? "Write the student account ["+ this.account +"]: " : "Write the student account: ");
+    this.account = inputKeyboard.nextLine();
+    System.out.println((this.firstName != null) ? "Write the student first name ["+ this.firstName +"]: " : "Write the student first name: ");
+    this.firstName = inputKeyboard.nextLine();
+    System.out.println("Write the student last name: ");
+    this.lastName = inputKeyboard.nextLine();
+    System.out.println("Write the student age: ");
+    this.age = inputKeyboard.nextInt();
+    inputKeyboard.nextLine();
+    System.out.println("Write the student carrer: ");
+    this.carrer = inputKeyboard.nextLine();
   }
 
   @Override
--- fundamentals/StudentsExcercise/src/main/java/classes/main.java
+++ fundamentals/StudentsExcercise/src/main/java/classes/main.java
@@ -1,56 +1,63 @@
 package classes;
-
+// 49
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class main {
+  
+  public int option;
+  public Scanner inputKeyboard = new Scanner(System.in);
+  public ArrayList<Student> students = new ArrayList<Student>();
 
-  public static void main(String[] args) {
+  public void addNewStudent() {
+    Student newStudent = new Student();
+    newStudent.addNewStudent(inputKeyboard);
+    students.add(newStudent);
+  }
 
-    Scanner inputKeyboard = new Scanner(System.in);
-    ArrayList students = new ArrayList<Student>();
-    int age;
-    int option;
-    String account;
-    String firstName;
-    String lastName;
-    String carrer;
+  public void viewAllStudents() {
+    students.forEach(student -> {
+      System.out.println(student.toString());
+    });
+  }
 
+  public void deleteAlumno() {
+    int studentSelected;
+    System.out.println("Write account of student do you want to delete (0-" + (students.size() - 1) + "): ");
+    studentSelected = inputKeyboard.nextInt();
+    inputKeyboard.nextLine();
+    students.remove(studentSelected);
+    System.out.println("Student Deleted!!!");
+  }
+  
+  public void updateStudent () {
+    int studentSelected;
+    System.out.println("Write account of student do you want to delete (0-" + (students.size() - 1) + "): ");
+    studentSelected = inputKeyboard.nextInt();
+    inputKeyboard.nextLine();
+    students.get(studentSelected).addNewStudent(inputKeyboard);
+  }
+  
+  public main() {
     do {
       System.out.println("UNAH STUDENTS CONTROL: \n1. Add Student\n2. Update Student\n3. Delete Student\n4. View All Students\n0. Exit\nWrite an option: ");
       option = inputKeyboard.nextInt();
       inputKeyboard.nextLine();
 
       switch (option) {
-        case 1:
-          System.out.println("===ADDING NEW STUDENT===");
-          System.out.println("Write the student account: ");
-          account = inputKeyboard.nextLine();
-          System.out.println("Write the student first name: ");
-          firstName = inputKeyboard.nextLine();
-          System.out.println("Write the student last name: ");
-          lastName = inputKeyboard.nextLine();
-          System.out.println("Write the student age: ");
-          age = inputKeyboard.nextInt();
-          inputKeyboard.nextLine();
-          System.out.println("Write the student carrer: ");
-          carrer = inputKeyboard.nextLine();
-          students.add(new Student(account, firstName, lastName, age, carrer));
-          break;
-        case 2:
-          break;
-        case 3:
-          break;
-        case 4:
-          students.forEach(student -> {
-            System.out.println(student.toString());
-          });
-          break;
-        default:
-          System.out.println("Option Not Valid!");
-          break;
+        case 1 -> addNewStudent();
+        case 2 -> updateStudent();
+        case 3 -> deleteAlumno();
+        case 4 -> viewAllStudents();
+        case 0 -> System.out.println("Bye!!!");
+        default -> System.out.println("Option Not Valid!");
       }
     } while (option != 0);
     inputKeyboard.close();
+  }
+
+  public static void main(String[] args) {
+    // WTF how is that posible
+    new main();
   }
 }
